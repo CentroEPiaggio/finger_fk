bcbb847649162c38b8e3e8003ae309905649df7e
George Jose Pollayil <gpollayil@gmail.com>
2018-09-24 15:56:10 +0200

    Debugged and now test finger joint service is functioning

--- src/finger_joints_service.cpp
+++ src/finger_joints_service.cpp
@@ -26,7 +26,8 @@
 // OTHER INCLUDES
 #include <cmath>
 
-#define HAND_NAME		"right_hand"
+#define DEBUG	1
+#define HAND_NAME	"right_hand"
 
 using namespace std;
 
@@ -38,18 +39,22 @@ ros::NodeHandle* fjs_nh_ptr; 			// a pointer to the node handle for the callback
 **********************************************************************************************/
 sensor_msgs::JointState getFingerJointState(int finger_id_, bool &success_){
 	// Listening the whole joint state
+	if (DEBUG) ROS_INFO("STARTING TO LISTEN TO /joint_states!");
 	sensor_msgs::JointState::ConstPtr full_joint_state =
 		ros::topic::waitForMessage<sensor_msgs::JointState>("/joint_states", *fjs_nh_ptr);
 	if (!full_joint_state) {
 		ROS_ERROR("Finger joint states not received!!! \n");
 		success_ = false;
 	}
+	if (DEBUG) ROS_INFO("LISTENED SUCCESSFULLY TO /joint_states!");
 
 	// Creating a JointState to be filled up
 	sensor_msgs::JointState finger_joint_state;
 
 	// Saving the needed joint states of finger in collision
 	if (finger_id_ == 1) {
+
+		if (DEBUG) ROS_INFO("STARTING TO FILL UP THUMB JOINTS!");
 
 		int index = find (full_joint_state->name.begin(),full_joint_state->name.end(),
 			string(HAND_NAME) + "_thumb_abd_joint") - full_joint_state->name.begin();
@@ -76,7 +81,11 @@ sensor_msgs::JointState getFingerJointState(int finger_id_, bool &success_){
 		finger_joint_state.name.push_back(full_joint_state->name[index]);
 		finger_joint_state.position.push_back(full_joint_state->position[index]);
 
+		if (DEBUG) ROS_INFO("FINISHED TO FILL UP THUMB JOINTS!");
+
 	} else if (finger_id_ == 2) {
+
+		if (DEBUG) ROS_INFO("STARTING TO FILL UP INDEX JOINTS!");
 
 		int index = find (full_joint_state->name.begin(),full_joint_state->name.end(),
 			string(HAND_NAME) + "_index_abd_joint") - full_joint_state->name.begin();
@@ -113,7 +122,11 @@ sensor_msgs::JointState getFingerJointState(int finger_id_, bool &success_){
 		finger_joint_state.name.push_back(full_joint_state->name[index]);
 		finger_joint_state.position.push_back(full_joint_state->position[index]);
 
+		if (DEBUG) ROS_INFO("FINISHED TO FILL UP INDEX JOINTS!");
+
 	} else if (finger_id_ == 3) {
+
+		if (DEBUG) ROS_INFO("STARTING TO FILL UP MIDDLE JOINTS!");
 
 		int index = find (full_joint_state->name.begin(),full_joint_state->name.end(),
 			string(HAND_NAME) + "_middle_abd_joint") - full_joint_state->name.begin();
@@ -150,7 +163,11 @@ sensor_msgs::JointState getFingerJointState(int finger_id_, bool &success_){
 		finger_joint_state.name.push_back(full_joint_state->name[index]);
 		finger_joint_state.position.push_back(full_joint_state->position[index]);
 
+		if (DEBUG) ROS_INFO("FINISHED TO FILL UP MIDDLE JOINTS!");
+
 	} else if (finger_id_ == 4) {
+
+		if (DEBUG) ROS_INFO("STARTING TO FILL UP RING JOINTS!");
 
 		int index = find (full_joint_state->name.begin(),full_joint_state->name.end(),
 			string(HAND_NAME) + "_ring_abd_joint") - full_joint_state->name.begin();
@@ -187,7 +204,11 @@ sensor_msgs::JointState getFingerJointState(int finger_id_, bool &success_){
 		finger_joint_state.name.push_back(full_joint_state->name[index]);
 		finger_joint_state.position.push_back(full_joint_state->position[index]);
 
+		if (DEBUG) ROS_INFO("FINISHED TO FILL UP RING JOINTS!");
+
 	} else if (finger_id_ == 5) {
+
+		if (DEBUG) ROS_INFO("STARTING TO FILL UP LITTLE JOINTS!");
 
 		int index = find (full_joint_state->name.begin(),full_joint_state->name.end(),
 			string(HAND_NAME) + "_little_abd_joint") - full_joint_state->name.begin();
@@ -224,11 +245,14 @@ sensor_msgs::JointState getFingerJointState(int finger_id_, bool &success_){
 		finger_joint_state.name.push_back(full_joint_state->name[index]);
 		finger_joint_state.position.push_back(full_joint_state->position[index]);
 
+		if (DEBUG) ROS_INFO("FINISHED TO FILL UP LITTLE JOINTS!");
+
 	} else {
 		ROS_WARN("The Finger Joint Server ready to process requests!");
 		success_ = false;
 	}
 	success_ = true;
+	return finger_joint_state;
 }
 
 /**********************************************************************************************
@@ -243,7 +267,9 @@ bool getFingerJoints(finger_fk::FingerJointsService::Request &req,
 		sensor_msgs::JointState finger_joints = getFingerJointState(finger_id, success);
 
 		// Filling up the Response
+		if (DEBUG) ROS_INFO("FILLING UP THE RESPONSE!");
 		res.joint_state = finger_joints;
+		if (DEBUG) ROS_INFO("FINISHED FILLING UP THE RESPONSE!");
 
 		// Ending the callback
 		return success;
